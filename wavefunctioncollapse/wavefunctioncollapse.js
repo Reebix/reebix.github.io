f30cc3f46b5fee047e37ff3d6176e6e81474fe9d
Rebix <timon@set-richter.de>
2023-06-03 18:07:42 +0200

    c

--- wavefunctioncollapse/wavefunctioncollapse.js
+++ wavefunctioncollapse/wavefunctioncollapse.js
@@ -1,4 +1,4 @@
-const size = 25; //size of tiles
+const size = 20; //size of tiles
 class tile {
   constructor(sides, pos = [0,0]) {
     this.neighbours = [];
@@ -19,25 +19,47 @@ class tile {
   }
 
   draw() {
+stroke(0)
+fill(255)
     rect(this.pos[0] * size, this.pos[1] * size,size,size);
-  }
-}
+    fill(0,255,0);
+    stroke(0,255,0);
 
-let tiles = [new tile([[0,1,0],[0,1,0],[0,1,0],[0,1,0]])]; //array of tiles
+}
+}
+let tiles = [new tile()]; //array of tiles
 let tileMap = []; //array of tiles
 
 function setup() {
+  tileMap = new Map();
   createCanvas(400, 400);
+  /*
   for (let i = 0; i < width / size; i++) {
     for (let j = 0; j < height / size; j++) {
+      if(random(1) < 0.5){
       let n = tiles[0].create();
   n.pos = [i,j];
   tileMap.push(n);
+      }
     }
+   
   }
+   */
 }
 
 function draw() {
-  background(5);
+
+  background(0);
 tileMap.forEach(tile => tile.draw());
+}
+
+function mousePressed() {
+ randomTile();
+}
+
+function randomTile() {
+  let t = tiles[0].create();
+  let pos = [floor(random(width / size)), floor(random(height / size))];
+  t.pos = pos;
+  tileMap.set(pos, t);
 }
